1f6a53fe35472119431493c17a4a3f7c2c8fd201
unknown <carterpaccione@gmail.com>
2024-10-02 17:52:27 -0400

    test

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,49 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('loggedUser') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('loggedUser', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('loggedUser');
+    window.location.assign('/');
   }
 }
 
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,22 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization; // Bearer token
+
+  if (authHeader) { // if the auth header exists
+    const token = authHeader.split(' ')[1]; // the token is the first part of the string
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
