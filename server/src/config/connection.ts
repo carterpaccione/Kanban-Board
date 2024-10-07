3b3b390d7fc08cfc0705547b137633205551e50d
unknown <carterpaccione@gmail.com>
2024-10-07 19:26:56 -0400

    ternary connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,32 +1,66 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import { Sequelize } from 'sequelize';
+
+// let sequelize;
+
+// if (process.env.DATABASE_URL) {
+//     console.log('PRODUCTION INSTANCE');
+//     sequelize = new Sequelize(process.env.DATABASE_URL, {
+//         dialect: 'postgres',
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false,
+//             },
+//         },
+//     })
+// } else {
+//     console.log('FAIL')
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME || 'kanban_db',
+//         process.env.DB_USER || 'postgres',
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'postgres',
+//         },
+//     );
+// }
+
+// export default sequelize;
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-let sequelize;
-
-if (process.env.DATABASE_URL) {
-    console.log('PRODUCTION INSTANCE');
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true, // Enforce SSL
+          rejectUnauthorized: false, // This allows self-signed certificates, if any
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
         dialect: 'postgres',
         dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
+          decimalNumbers: true,
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
         },
-    })
-} else {
-    console.log('FAIL')
-    sequelize = new Sequelize(
-        process.env.DB_NAME || 'kanban_db',
-        process.env.DB_USER || 'postgres',
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'postgres',
-        },
+      },
     );
-}
 
 export default sequelize;
