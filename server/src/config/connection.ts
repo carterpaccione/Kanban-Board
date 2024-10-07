13d2ba1e5cb5f4a41f8b1c62369fcf690b78d91b
unknown <carterpaccione@gmail.com>
2024-10-07 18:57:23 -0400

    seqleize dialect options

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -6,7 +6,15 @@ import { Sequelize } from 'sequelize';
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL);
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    })
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME || 'kanban_db',
